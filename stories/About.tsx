2408ac06f7f724c810686b7613ce5136820874b5
Matej <ematej.snuderl@gmail.com>
2019-03-09 13:36:24 +0100

    Storybook

---
+++ stories/About.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+import About from '../pages/about';
+
+storiesOf('About stories', module).add('Default story', () => <About />);
